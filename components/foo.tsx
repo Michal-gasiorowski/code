7969c209fc16f9b90e98ff7b303844771ba9199e
Michal-gasiorowski <michal.gitara@gmail.com>
2023-08-28 14:59:12 +0200

    variadic function

--- components/foo.tsx
+++ components/foo.tsx
@@ -1,41 +1,13 @@
-import { createContext, useContext, useState } from "react";
+function variadicFnc<T>(
+  callback: (...args: [string, ...T[], boolean]) => void,
+  ...args: [...Parameters<typeof callback>]
+) {
+  return callback(...args);
+}
 
-import { AppWrapper } from "#components";
+variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1, true);
 
-export const ParentContext = createContext({ count: 0 });
+variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1, true, "oops");
+variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1);
 
-export const ShowCount = () => {
-  const { count } = useContext(ParentContext);
-  return <div>count: {count}</div>;
-};
-
-export const AddTwo = () => {
-  const { count } = useContext(ParentContext);
-  return <div>count plus two: {count + 2}</div>;
-};
-
-export const ParentComponent = ({ children }) => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <ParentContext.Provider value={{ count }}>
-      {children}
-      <button onClick={() => setCount((state) => state + 1)}>Add one</button>
-    </ParentContext.Provider>
-  );
-};
-
-ParentComponent.AddTwo = AddTwo;
-ParentComponent.ShowCount = ShowCount;
-
-export const Parent = ParentComponent;
-
-export const Wrapper = () => (
-  <Parent>
-    <Parent.ShowCount />
-  </Parent>
-);
-
-<AppWrapper>
-  <Wrapper />
-</AppWrapper>;
+variadicFnc((a: string, b: string, c: boolean) => {}, "hello", 1, true);
