0570b9e858eb814d7d70d8a5db0251798c76398e
Michal-gasiorowski <michal.gitara@gmail.com>
2023-08-30 20:30:31 +0200

    push

--- components/foo.tsx
+++ components/foo.tsx
@@ -1,15 +1,1 @@
-function variadicFnc<T>(
-  callback: (...args: [string, ...T[], boolean]) => void,
-  ...args: [...Parameters<typeof callback>]
-) {
-  return callback(...args);
-}
-
-variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1, true);
-
-// variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1, true, "oops");
-// variadicFnc((a: string, b: number, c: boolean) => {}, "hello", 1);
-
-// variadicFnc((a: string, b: string, c: boolean) => {}, "hello", 1, true);
-
 export {};
